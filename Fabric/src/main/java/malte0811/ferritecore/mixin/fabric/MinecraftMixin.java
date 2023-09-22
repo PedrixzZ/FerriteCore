ac037ea2ad9fd60580ce67a65554a9ff3dd88803
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-09-22 10:42:44 -0300

    Update MinecraftMixin.java

--- Fabric/src/main/java/malte0811/ferritecore/mixin/fabric/MinecraftMixin.java
+++ Fabric/src/main/java/malte0811/ferritecore/mixin/fabric/MinecraftMixin.java
@@ -3,6 +3,7 @@ package malte0811.ferritecore.mixin.fabric;
 import malte0811.ferritecore.impl.Deduplicator;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.main.GameConfig;
+import net.minecraft.client.renderer.item.RenderItem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,10 +15,10 @@ public class MinecraftMixin {
             method = "<init>",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/client/renderer/entity/ItemRenderer;<init>(Lnet/minecraft/client/Minecraft;Lnet/minecraft/client/renderer/texture/TextureManager;Lnet/minecraft/client/resources/model/ModelManager;Lnet/minecraft/client/color/item/ItemColors;Lnet/minecraft/client/renderer/BlockEntityWithoutLevelRenderer;)V"
+                    target = "Lnet/minecraft/client/renderer/entity/ItemRenderer;<init>(Lnet/minecraft/client/Minecraft;Lnet/minecraft/client/renderer/texture/TextureManager;Lnet/minecraft/client/resources/model/ModelManager;Lnet/minecraft/client/color/item/ItemColors;Lnet/minecraft/client/renderer/BlockEntityWithoutLevelRenderer;Lnet/minecraft/client/renderer/item/RenderItem;Lnet/minecraft/client/renderer/entity/EntityRendererManager;Lnet/minecraft/client/renderer/entity/LivingEntityRenderer;Lnet/minecraft/client/renderer/entity/MobEntityRenderer;Lnet/minecraft/client/renderer/entity/PlayerEntityRenderer;Lnet/minecraft/world/level/block/entity/BlockEntityRenderer;Lnet/minecraft/client/renderer/chunk/ChunkRenderDispatcher;)V"
             )
     )
-    private void injectAfterModels(GameConfig gameConfig, CallbackInfo ci) {
-        Deduplicator.registerReloadListener();
+    private void injectAfterModels(GameConfig gameConfig, RenderItem renderItem, CallbackInfo ci) {
+        Deduplicator.registerReloadListener(renderItem);
     }
 }
